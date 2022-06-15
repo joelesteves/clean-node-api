6b84476271b7e6911db8c0ca369fe41f6d6a14f0
joel esteves <joel.esteves@hotmail.com>
2022-06-15 17:00:38 -0300

    test: ensure SingnupController return an error if no name is provided

---
+++ src/presentation/controllers/singnup.spec.ts
@@ -0,0 +1,17 @@
+import { SingnupController } from '../singnup'
+
+describe('', () => {
+  test('should return 400 if no name provided', () => {
+    const sut = new SingnupController()
+    const httpRequest = {
+      body: {
+        email: 'any@email.com',
+        password: 'anypassword',
+        passwordConfirmation: 'anypassword'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+})
