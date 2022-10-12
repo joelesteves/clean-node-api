7c911a95dd65c6a485f6c4d83c6de1b697b01418
joelesteves <joel.esteves@hotmail.com>
2022-10-12 14:48:25 -0300

    test: adding error handling and http helper method

--- src/presentation/controllers/singnup.spec.ts
+++ src/presentation/controllers/singnup.spec.ts
@@ -1,4 +1,5 @@
 import { SingnupController } from './singnup'
+import { MissingParamError } from '../error/missing-parm-error'
 
 describe('', () => {
   test('should return 400 if no name provided', () => {
@@ -12,7 +13,7 @@ describe('', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('should return 400 if no email provided', () => {
@@ -26,6 +27,6 @@ describe('', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
