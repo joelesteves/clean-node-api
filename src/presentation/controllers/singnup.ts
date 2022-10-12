26f930192199fadb4027adfdbdf07cc243de763c
joelesteves <joel.esteves@hotmail.com>
2022-10-12 14:46:35 -0300

    refactor: adding error handling and http helper method

--- src/presentation/controllers/singnup.ts
+++ src/presentation/controllers/singnup.ts
@@ -1,16 +1,14 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../error/missing-parm-error'
+import { badRequest } from '../helpers/http-help'
+
 export class SingnupController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse | any {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: email')
-      }
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
